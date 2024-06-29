c2bc3beb8b5f75bda6244600d7bdb971f1ebb449
leeleeleeleejun <wnstjr6293@naver.com>
2024-06-29 15:23:45 +0900

    feat: marker클릭 시 활성화 ui 추가 - marker 컴포넌트 분리

---
+++ src/components/common/MarkerContainer/index.tsx
@@ -0,0 +1,46 @@
+import { Marker, useNavermaps } from 'react-naver-maps';
+import { MarkerContainerProps } from '@/types/props';
+
+const MarkerContainer = ({
+  position,
+  title,
+  activeMarker,
+  setActiveMarker,
+}: MarkerContainerProps) => {
+  const naverMaps = useNavermaps();
+
+  return (
+    <Marker
+      position={new naverMaps.LatLng(position[0], position[1])}
+      title={title}
+      icon={{
+        content: MarkerIcon(title, activeMarker),
+      }}
+      onClick={(e) => {
+        e.pointerEvent.stopPropagation();
+        setActiveMarker(title);
+      }}
+    />
+  );
+};
+
+export default MarkerContainer;
+
+const MarkerIcon = (place: string, activeMarker: null | string) => {
+  const content = place ? `${place} <span>도착</span>` : '도착';
+  const isActive =
+    activeMarker === null
+      ? 'activeMarker'
+      : activeMarker === place
+        ? 'activeMarker'
+        : 'nonActiveMarker';
+
+  return `
+    <div class="marker-icon-container ${isActive}">
+      <div class="marker-content-box">
+        ${content}
+      </div>
+      <div class="marker-bottom-triangle"></div>
+    </div>
+  `;
+};
