234e662bd0e2e9a63aa8ac3d114bc728061df014
leeleeleeleejun <wnstjr6293@naver.com>
2024-07-29 21:48:44 +0900

    feat: page전체 lazy loading 적용

--- src/Router.tsx
+++ src/Router.tsx
@@ -2,31 +2,43 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import HomePage from '@/pages/HomePage';
 import Layout from '@/components/common/Layout';
-import SearchPage from '@/pages/SearchPage.tsx';
-import PostDetailPage from '@/pages/PostDetailPage.tsx';
-import CreatePostPage from '@/pages/CreatePostPage';
-import ChatListPage from '@/pages/ChatListPage.tsx';
-import ChatRoomPage from '@/pages/ChatRoomPage.tsx';
-import UsageHistoryPage from '@/pages/UsageHistoryPage.tsx';
-import MyProfilePage from '@/pages/MyProfilePage.tsx';
-import LoginPage from '@/pages/LoginPage.tsx';
+//import SearchPage from '@/pages/SearchPage.tsx';
+//import PostDetailPage from '@/pages/PostDetailPage.tsx';
+//import CreatePostPage from '@/pages/CreatePostPage';
+//import ChatListPage from '@/pages/ChatListPage.tsx';
+//import ChatRoomPage from '@/pages/ChatRoomPage.tsx';
+//import UsageHistoryPage from '@/pages/UsageHistoryPage.tsx';
+//import MyProfilePage from '@/pages/MyProfilePage.tsx';
+//import LoginPage from '@/pages/LoginPage.tsx';
+import { lazy, Suspense } from 'react';
+
+const SearchPage = lazy(() => import('@/pages/SearchPage'));
+const PostDetailPage = lazy(() => import('@/pages/PostDetailPage'));
+const CreatePostPage = lazy(() => import('@/pages/CreatePostPage'));
+const ChatListPage = lazy(() => import('@/pages/ChatListPage'));
+const ChatRoomPage = lazy(() => import('@/pages/ChatRoomPage'));
+const UsageHistoryPage = lazy(() => import('@/pages/UsageHistoryPage'));
+const MyProfilePage = lazy(() => import('@/pages/MyProfilePage'));
+const LoginPage = lazy(() => import('@/pages/LoginPage'));
 
 const Router = () => {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route element={<Layout />}>
-          <Route path={'/'} element={<HomePage />} />
-          <Route path={'/search'} element={<SearchPage />} />
-          <Route path={'/post-detail'} element={<PostDetailPage />} />
-          <Route path={'/create-post'} element={<CreatePostPage />} />
-          <Route path={'/chat-list'} element={<ChatListPage />} />
-          <Route path={'/chat-list/a'} element={<ChatRoomPage />} />
-          <Route path={'/usage-history/'} element={<UsageHistoryPage />} />
-          <Route path={'/my-profile/'} element={<MyProfilePage />} />
-          <Route path={'/login'} element={<LoginPage />} />
-        </Route>
-      </Routes>
+      <Suspense fallback={<div> 로딩중... </div>}>
+        <Routes>
+          <Route element={<Layout />}>
+            <Route path={'/'} element={<HomePage />} />
+            <Route path={'/search'} element={<SearchPage />} />
+            <Route path={'/post-detail'} element={<PostDetailPage />} />
+            <Route path={'/create-post'} element={<CreatePostPage />} />
+            <Route path={'/chat-list'} element={<ChatListPage />} />
+            <Route path={'/chat-list/a'} element={<ChatRoomPage />} />
+            <Route path={'/usage-history/'} element={<UsageHistoryPage />} />
+            <Route path={'/my-profile/'} element={<MyProfilePage />} />
+            <Route path={'/login'} element={<LoginPage />} />
+          </Route>
+        </Routes>
+      </Suspense>
     </BrowserRouter>
   );
 };
--- src/constants/index.ts
+++ src/constants/index.ts
@@ -3,16 +3,16 @@ import { SelectionKey } from '@/types';
 
 export const postData = () => {
   const line = [
-    { order: 11, station: '잠실새내', code: [37.5115735, 127.0868931] },
-    { order: 23, station: '종합운동장', code: [37.5110588, 127.0737908] },
-    { order: 1456, station: '삼성', code: [37.5088803, 127.0631076] },
-    { order: 71, station: '선릉', code: [37.504585, 127.0492805] },
-    { order: 1341, station: '역삼', code: [37.5006431, 127.0363764] },
-    { order: 65, station: '강남', code: [37.4979526, 127.0276241] },
-    { order: 333, station: '교대', code: [37.4939732, 127.0146391] },
-    { order: 575, station: '방배', code: [37.4814955, 126.9976669] },
-    { order: 3, station: '사당', code: [37.4765793, 126.981596] },
-    { order: 578, station: '신대방', code: [37.4875672, 126.9133456] },
+    // { order: 11, station: '잠실새내', code: [37.5115735, 127.0868931] },
+    // { order: 23, station: '종합운동장', code: [37.5110588, 127.0737908] },
+    // { order: 1456, station: '삼성', code: [37.5088803, 127.0631076] },
+    // { order: 71, station: '선릉', code: [37.504585, 127.0492805] },
+    // { order: 1341, station: '역삼', code: [37.5006431, 127.0363764] },
+    // { order: 65, station: '강남', code: [37.4979526, 127.0276241] },
+    // { order: 333, station: '교대', code: [37.4939732, 127.0146391] },
+    // { order: 575, station: '방배', code: [37.4814955, 126.9976669] },
+    // { order: 3, station: '사당', code: [37.4765793, 126.981596] },
+    // { order: 578, station: '신대방', code: [37.4875672, 126.9133456] },
     {
       order: 976,
       station: '구로디지털단지구로디지털단지',
