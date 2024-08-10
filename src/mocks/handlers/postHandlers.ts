cfb7f027cc53ea2c89c51aedba6595bf8e49bebb
leeleeleeleejun <wnstjr6293@naver.com>
2024-08-10 14:36:14 +0900

    faet: 팟 생성  mockdata 생성

--- src/mocks/handlers/postHandlers.ts
+++ src/mocks/handlers/postHandlers.ts
@@ -1,6 +1,6 @@
 import { http, HttpResponse } from 'msw';
 import { API_PATH } from '@/constants/path.ts';
-import { posts } from '@/mocks/data/post.ts';
+import { posts, creatPost } from '@/mocks/data/post.ts';
 
 const PostHandlers = [
   // 범위 내 팟 조회
@@ -17,6 +17,11 @@ const PostHandlers = [
   }),
   http.get(API_PATH.POST.GET.CLOSE_POSTS, () => {
     return HttpResponse.json(posts);
+  }),
+
+  //팟 생성
+  http.post(API_PATH.POST.POST, () => {
+    return HttpResponse.json(creatPost);
   }),
 ];
 
