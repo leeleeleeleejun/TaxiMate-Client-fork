29ae88acd64c40563863832dd9b230c56abbc4c2
leeleeleeleejun <wnstjr6293@naver.com>
2024-09-21 17:28:27 +0900

    a

--- src/components/chatRoom/MessageList.tsx
+++ src/components/chatRoom/MessageList.tsx
@@ -4,18 +4,18 @@ import {
   Container,
   SystemMessage,
 } from '@/components/chatRoom/chatRoom.style.ts';
-import { useLayoutEffect, useRef, useState } from 'react';
+import { useLayoutEffect, useRef } from 'react';
 
-interface testChat {
-  _id: string;
-  chat: string;
-  createdAt: string;
-  updatedAt: string;
-  user: {
-    id: string;
-    name: string;
-  };
-}
+// interface testChat {
+//   _id: string;
+//   chat: string;
+//   createdAt: string;
+//   updatedAt: string;
+//   user: {
+//     id: string;
+//     name: string;
+//   };
+// }
 
 interface groupMessage {
   _id: string;
@@ -30,37 +30,16 @@ interface groupMessage {
 
 // Todo: userName은 id로 변경 필요
 const MessageList = () => {
-  const [messageList, setMessageList] = useState<groupMessage[]>([]);
+ // const [messageList, setMessageList] = useState<groupMessage[]>([]);
   const messageEndRef = useRef<HTMLDivElement>(null);
 
-  const handleMessage = (message: testChat) => {
-    const setMessage = { ...message, chat: [message.chat] };
-
-    setMessageList((prevState) => {
-      if (prevState.length > 0) {
-        const lastMessage = prevState[prevState.length - 1];
-        const isSameUser = lastMessage.user.id === message.user.id;
-        const isSameTime =
-          lastMessage?.createdAt.slice(0, 16) ===
-          message.createdAt.slice(0, 16);
-
-        if (isSameUser && isSameTime) {
-          const updatedMessage = {
-            ...lastMessage,
-            chat: [...lastMessage.chat, ...setMessage.chat],
-          };
-          return [...prevState.slice(0, prevState.length - 1), updatedMessage];
-        }
-      }
-      return [...prevState, setMessage];
-    });
-  };
 
   // useEffect(() => {
   //   socket.on('message', (message: testChat) => {
   //     handleMessage(message);
   //   });
   // }, []);
+const messageList:groupMessage[] = [];
 
   useLayoutEffect(() => {
     messageEndRef.current?.scrollIntoView({ behavior: 'smooth' });
