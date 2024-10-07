50469ce1d9d4faf7226d3704e7e7be8d5b3a6c9b
leeleeleeleejun <wnstjr6293@naver.com>
2024-10-07 11:57:10 +0900

    path 수정

--- src/constants/path.ts
+++ src/constants/path.ts
@@ -1,4 +1,4 @@
-export const LocalAPI = 'http://localhost:5173';
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 export const API_PATH = {
   SEARCH: {
@@ -6,25 +6,25 @@ export const API_PATH = {
   },
   POST: {
     GET: {
-      ALL: `/api/v1/parties`,
+      ALL: API_BASE_URL + `/api/v1/parties`,
       // 포스트 ID 조회
-      BY_ID: `/api/v1/parties/:partyId`,
+      BY_ID: API_BASE_URL + `/api/v1/parties/:partyId`,
       // 특정 유저의 전체 포스트 조회
-      BY_USER: `/posts/user/:userId`,
-      JOIN_POSTS: `${LocalAPI}/api/v1/participation`,
-      CLOSE_POSTS: `${LocalAPI}/api/v1/participation?isTerminated=true`,
+      BY_USER: API_BASE_URL + `/posts/user/:userId`,
+      JOIN_POSTS: API_BASE_URL + `/api/v1/participation`,
+      CLOSE_POSTS: API_BASE_URL + `/api/v1/participation?isTerminated=true`,
     },
-    POST: `/api/v1/parties`,
+    POST: API_BASE_URL + `/api/v1/parties`,
   },
   USER: {
-    GET_ACCESS_TOKEN: `/oauth2/kakao`,
-    GET_REFRESH_ACCESS_TOKEN: `/api/v1/auth/tokens`,
-    GET_PROFILES: `/api/v1/profiles`,
+    GET_ACCESS_TOKEN: API_BASE_URL + `/oauth2/kakao`,
+    GET_REFRESH_ACCESS_TOKEN: API_BASE_URL + `/api/v1/auth/tokens`,
+    GET_PROFILES: API_BASE_URL + `/api/v1/profiles`,
   },
   CHAT: {
-    PARTICIPATION: `/api/v1/participation`,
-    GET_CHAT_LIST: `/api/v1/chats`,
-    GET_CHAT: '/api/v1/chats/:partyId',
+    PARTICIPATION: API_BASE_URL + `/api/v1/participation`,
+    GET_CHAT_LIST: API_BASE_URL + `/api/v1/chats`,
+    GET_CHAT: API_BASE_URL + '/api/v1/chats/:partyId',
   },
 };
 
