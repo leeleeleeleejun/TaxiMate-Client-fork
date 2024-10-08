a88abfb25ef6c0830c0424fb430fbfccfb615b27
leeleeleeleejun <wnstjr6293@naver.com>
2024-10-08 20:42:46 +0900

    design: 채티방 title margin-right 추가

--- src/components/chatRoom/chatRoom.style.ts
+++ src/components/chatRoom/chatRoom.style.ts
@@ -2,6 +2,8 @@ import styled from 'styled-components';
 import { Link } from 'react-router-dom';
 
 export const RoomTitle = styled.h2`
+  margin-right: 34px;
+
   flex-grow: 1;
   text-align: center;
   font-size: 18px;
