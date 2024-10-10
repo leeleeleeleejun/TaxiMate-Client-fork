9896c03bde6c2bb17ca88b44c52641f37279592c
leeleeleeleejun <wnstjr6293@naver.com>
2024-10-10 23:48:50 +0900

    remove: 알림설정 컴포넌트 주석처리

--- src/pages/MyProfilePage.tsx
+++ src/pages/MyProfilePage.tsx
@@ -8,8 +8,8 @@ import MyProfileIcon from '@/assets/icons/header/my-porfile-icon.svg?react';
 import { useGetProfileQuery } from '@/api/localApi.ts';
 import MenuItem from '@/components/myProfile/MenuItem.tsx';
 import FileIcon from '@/assets/icons/file-icon.svg?react';
-import NoticeIcon from '@/assets/icons/notice-icon.svg?react';
-import Toggle from '@/components/common/Toggle.tsx';
+// import NoticeIcon from '@/assets/icons/notice-icon.svg?react';
+// import Toggle from '@/components/common/Toggle.tsx';
 import LoadingIcon from '@/components/common/LoadingIcon';
 
 const MyProfilePage = () => {
@@ -28,9 +28,9 @@ const MyProfilePage = () => {
       <Container>
         <UserContainer img={data.profileImage} name={data.nickname} />
         <Menu>
-          <MenuItem content={'알림설정'} SvgIcon={NoticeIcon}>
-            <Toggle />
-          </MenuItem>
+          {/*<MenuItem content={'알림설정'} SvgIcon={NoticeIcon}>*/}
+          {/*  <Toggle />*/}
+          {/*</MenuItem>*/}
           <MenuItem content={'이용약관'} SvgIcon={FileIcon} />
         </Menu>
       </Container>
