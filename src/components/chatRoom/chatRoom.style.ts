977ded04ab8bb28179dd8300fe76f76c202f8d3c
leeleeleeleejun <wnstjr6293@naver.com>
2024-10-11 22:19:30 +0900

    fix: 채팅방 프로필 이미지 min-width 으로 설정

--- src/components/chatRoom/chatRoom.style.ts
+++ src/components/chatRoom/chatRoom.style.ts
@@ -98,8 +98,8 @@ export const MessageTime = styled.span`
 `;
 
 export const OthersProfile = styled.img`
-  width: 40px;
-  height: 40px;
+  min-width: 40px;
+  min-height: 40px;
   border-radius: 50%;
 
   margin-right: 10px;
