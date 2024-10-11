b19356d844c4479e38ffc66490888963652d9314
leeleeleeleejun <wnstjr6293@naver.com>
2024-10-11 12:42:22 +0900

    style: kakao type global.d.ts로 위치 이동

--- src/App.tsx
+++ src/App.tsx
@@ -11,12 +11,6 @@ import GlobalStyle from '@/styles/GlobalStyle.ts';
 const naverMapApi = import.meta.env.VITE_NAVER_MAP_API;
 const kakaoJsKey = import.meta.env.VITE_KAKAO_JS_KEY;
 
-declare global {
-  interface Window {
-    Kakao: any;
-  }
-}
-
 window.Kakao.init(kakaoJsKey);
 
 // SDK 초기화 여부를 판단합니다.
--- src/global.d.ts
+++ src/global.d.ts
@@ -2,4 +2,5 @@ interface Window {
   ReactNativeWebView: {
     postMessage: (message: string) => void;
   };
+  Kakao: any;
 }
