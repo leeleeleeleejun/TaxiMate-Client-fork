78409d84512f092adb836a40568fabac6f01e6db
leeleeleeleejun <wnstjr6293@naver.com>
2024-10-11 17:30:26 +0900

    fix: navigate 위치 수정

--- src/components/common/Layout/index.tsx
+++ src/components/common/Layout/index.tsx
@@ -1,7 +1,25 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet, useNavigate } from 'react-router-dom';
 import Container from '@/components/common/Layout/Layout.style.ts';
+import { useEffect } from 'react';
+import { CLIENT_PATH } from '@/constants/path.ts';
+import reactNativePostMessage from '@/utils/postMessage.ts';
 
 const Layout = () => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const handleMessage = (e: MessageEvent) => {
+      console.log('Received message', e);
+      const { partyId } = JSON.parse(e.data);
+      if (partyId) {
+        navigate(CLIENT_PATH.CHAT_ROOM.replace(':chatRoomId', partyId));
+      }
+    };
+    window.addEventListener('message', handleMessage);
+    reactNativePostMessage('chat');
+
+    return () => window.removeEventListener('message', handleMessage);
+  }, []);
   return (
     <Container>
       <Outlet />
