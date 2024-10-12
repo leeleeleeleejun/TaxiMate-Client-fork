93a2c0360dbaa40489c76f90686cd7981c5e3ed3
leeleeleeleejun <wnstjr6293@naver.com>
2024-10-12 21:53:40 +0900

    fix: 로그인 로직 수정

--- src/pages/LoginLoadingPage.tsx
+++ src/pages/LoginLoadingPage.tsx
@@ -14,11 +14,16 @@ const LoginLoadingPage = () => {
   const location = useLocation();
   const code = new URLSearchParams(location.search).get('code') || '';
 
-  const { isLoading: isTokenLoading, isSuccess: isTokenSuccess } =
-    useGetAccessTokenQuery({ code: code });
+  const {
+    isLoading: isTokenLoading,
+    isSuccess: isTokenSuccess,
+    isError: isTokenError,
+    error: tokenError,
+  } = useGetAccessTokenQuery({ code });
+
   const [
     setPushAlarmTrigger,
-    { isLoading: isPushAlarmLoading, isSuccess: isPushAlarmSuccess },
+    { isLoading: isPushAlarmLoading, isError: isPushAlarmError },
   ] = useSetPushAlarmMutation();
 
   useEffect(() => {
@@ -26,34 +31,39 @@ const LoginLoadingPage = () => {
       setPushAlarmTrigger(e.data);
     };
     window.addEventListener('message', handleMessage);
-
     return () => window.removeEventListener('message', handleMessage);
   }, []);
 
   useEffect(() => {
-    if (isTokenSuccess && !isTokenLoading) {
-      dispatch(setIsLogin(true));
-      reactNativePostMessage('push_notification');
-    }
-  }, [isTokenSuccess, isTokenLoading, dispatch]);
-
-  useEffect(() => {
-    if (!isTokenLoading && !isPushAlarmLoading) {
-      if (isTokenSuccess && isPushAlarmSuccess) {
+    if (!isTokenLoading) {
+      if (isTokenSuccess) {
         dispatch(setIsLogin(true));
         reactNativePostMessage('push_notification');
+      } else if (isTokenError) {
+        console.error('Login failed:', tokenError);
+        navigate('/login');
       }
-      // 모든 작업이 완료된 후 네비게이션 수행
-      navigate('/');
     }
   }, [
     isTokenLoading,
     isTokenSuccess,
+    isTokenError,
+    tokenError,
     dispatch,
     navigate,
-    isPushAlarmLoading,
-    isPushAlarmSuccess,
   ]);
+
+  // 푸쉬 토큰 요청 후
+  useEffect(() => {
+    if (!isPushAlarmLoading) {
+      if (isPushAlarmError) {
+        console.error('Failed to set push alarm');
+        // 푸시 알람 설정 실패 시 처리 (예: 사용자에게 알림)
+        alert('푸시알람 설정에 실패했습니다.');
+      }
+    }
+    navigate('/');
+  }, [isPushAlarmLoading, isPushAlarmError, navigate]);
 
   return null;
 };
