896410716a5e9f7e73608595c9dac7bfe6079b3e
leeleeleeleejun <wnstjr6293@naver.com>
2024-10-14 19:39:40 +0900

    test: 메세지 관련 주석 해제

--- src/pages/LoginLoadingPage.tsx
+++ src/pages/LoginLoadingPage.tsx
@@ -1,7 +1,7 @@
 import { useEffect } from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
-// import reactNativePostMessage from '@/utils/reactNativePostMessage.ts';
+import reactNativePostMessage from '@/utils/reactNativePostMessage.ts';
 import {
   useGetAccessTokenQuery,
   // useSetPushAlarmMutation,
@@ -35,24 +35,24 @@ const LoginLoadingPage = () => {
   useErrorHandle(tokenError);
 
   // push_notification을 보낸 후 메시지를 처리
-  // useEffect(() => {
-  //   // if (isPushNotificationSent) {
-  //   const handleMessage = (e: MessageEvent) => {
-  //     alert('로그인 로딩 페이지에서 받은 메시지 (푸시토큰): ' + e.data);
-  //     setPushAlarmTrigger(e.data);
-  //   };
-  //
-  //   window.addEventListener('message', handleMessage);
-  //   return () => window.removeEventListener('message', handleMessage);
-  //   // }
-  // }, []);
+  useEffect(() => {
+    // if (isPushNotificationSent) {
+    const handleMessage = (e: MessageEvent) => {
+      alert('로그인 로딩 페이지에서 받은 메시지 (푸시토큰): ' + e.data);
+      // setPushAlarmTrigger(e.data);
+    };
+
+    window.addEventListener('message', handleMessage);
+    // return () => window.removeEventListener('message', handleMessage);
+    // }
+  }, []);
 
   // 토큰이 성공적으로 받아졌을 때
   useEffect(() => {
     if (!isTokenLoading && isTokenSuccess) {
       dispatch(setIsLogin(true));
-      // reactNativePostMessage('push_notification');
-      // alert('로그인 상태 변경 및 push_notification 전송 완료');
+      reactNativePostMessage('push_notification');
+      alert('로그인 상태 변경 및 push_notification 전송 완료');
       navigate('/');
     } else if (isTokenError) {
       navigate('/login');
