dbcaab5548a2953f92e4be1ed0a555990b76716f
leeleeleeleejun <wnstjr6293@naver.com>
2024-10-23 20:21:08 +0900

    fix: loading icon 롤백

--- src/pages/MyProfilePage.tsx
+++ src/pages/MyProfilePage.tsx
@@ -5,18 +5,17 @@ import Footer from '@/components/common/Layout/Footer';
 import UserContainer from '@/components/common/UserContainer';
 import { Container, Menu } from '@/components/MyProfile/myProfile.style.ts';
 import { HeaderItem } from '@/components/common/Layout/Header/Header.style.ts';
-import { SpinLoadingIcon } from '@/components/common/LoadingIcon/LoadingIcon.style.ts';
+import LoadingIcon from '@/components/common/LoadingIcon';
 
 import MyProfileIcon from '@/assets/icons/header/my-porfile-icon.svg?react';
 import MenuItem from '@/components/MyProfile/MenuItem.tsx';
 import FileIcon from '@/assets/icons/file-icon.svg?react';
 // import NoticeIcon from '@/assets/icons/notice-icon.svg?react';
 // import Toggle from '@/components/common/Toggle.tsx';
-// import LoadingIcon from '@/components/common/LoadingIcon';
 
 const MyProfilePage = () => {
   const { data, isLoading } = useGetProfileQuery(null);
-  if (isLoading) return <SpinLoadingIcon />;
+  if (isLoading) return <LoadingIcon />;
   if (!data) return <div>no data...</div>;
 
   return (
