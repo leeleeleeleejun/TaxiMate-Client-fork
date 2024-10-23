181df89a17d4f915f21eb74f4840eb08e8dbc034
leeleeleeleejun <wnstjr6293@naver.com>
2024-10-23 20:05:00 +0900

    test: rtkqurey isLoading icon 변경테스트

--- src/pages/MyProfilePage.tsx
+++ src/pages/MyProfilePage.tsx
@@ -1,20 +1,22 @@
+import { useGetProfileQuery } from '@/api/userApi.ts';
+
 import Header from '@/components/common/Layout/Header';
 import Footer from '@/components/common/Layout/Footer';
 import UserContainer from '@/components/common/UserContainer';
 import { Container, Menu } from '@/components/MyProfile/myProfile.style.ts';
 import { HeaderItem } from '@/components/common/Layout/Header/Header.style.ts';
+import { SpinLoadingIcon } from '@/components/common/LoadingIcon/LoadingIcon.style.ts';
 
 import MyProfileIcon from '@/assets/icons/header/my-porfile-icon.svg?react';
-import { useGetProfileQuery } from '@/api/userApi.ts';
 import MenuItem from '@/components/MyProfile/MenuItem.tsx';
 import FileIcon from '@/assets/icons/file-icon.svg?react';
 // import NoticeIcon from '@/assets/icons/notice-icon.svg?react';
 // import Toggle from '@/components/common/Toggle.tsx';
-import LoadingIcon from '@/components/common/LoadingIcon';
+// import LoadingIcon from '@/components/common/LoadingIcon';
 
 const MyProfilePage = () => {
   const { data, isLoading } = useGetProfileQuery(null);
-  if (isLoading) return <LoadingIcon />;
+  if (isLoading) return <SpinLoadingIcon />;
   if (!data) return <div>no data...</div>;
 
   return (
