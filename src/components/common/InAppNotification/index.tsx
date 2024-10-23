a4ee70bcfde6c4510ff35df0c19376a4ea41f1f4
leeleeleeleejun <wnstjr6293@naver.com>
2024-10-23 21:32:43 +0900

    feat: 인앱알림 드래그로 사라지기 기능 추가

--- src/components/common/InAppNotification/index.tsx
+++ src/components/common/InAppNotification/index.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { ChatMessage } from '@/types/chat.ts';
 import { CLIENT_PATH } from '@/constants/path.ts';
 import UserBasicImg from '@/components/common/userBasicImg';
@@ -23,10 +24,28 @@ const InAppNotification = ({
   showNotification,
   setShowNotification,
 }: Props) => {
+  const [touchStartY, setTouchStartY] = useState(0);
+  const [touchEndY, setTouchEndY] = useState(0);
+
+  const handleTouchStart = (e: React.TouchEvent) => {
+    setTouchStartY(e.touches[0].clientY);
+  };
+
+  const handleTouchEnd = (e: React.TouchEvent) => {
+    setTouchEndY(e.changedTouches[0].clientY);
+
+    // 위로 드래그되었는지 감지 (음수로 떨어지는 것을 고려)
+    if (touchStartY - touchEndY > 50) {
+      setShowNotification();
+    }
+  };
+
   return (
     <Container
       to={CLIENT_PATH.CHAT_ROOM.replace(':chatRoomId', String(partyId))}
       onClick={setShowNotification}
+      onTouchStart={handleTouchStart}
+      onTouchEnd={handleTouchEnd}
       $show={showNotification}
     >
       {sender.profileImage ? (
